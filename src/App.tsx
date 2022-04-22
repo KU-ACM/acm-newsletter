24c247dcb8a59d6a1c38a17b5857a6206f310d4b
omergguler <oguler19@ku.edu.tr>
2022-04-22 23:34:33 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import EventCard, { EventData } from "./components/EventCard";
+
+const sampleDate: string = "25 April, Monday";
+
+const sampleEvent: EventData = {
+  date: sampleDate,
+  title: "Ask Alumni - Berker Soyluoğlu",
+  description:
+    "Ask Alumni is here once again! This time in Ask Alumni, Berker Soyluoğlu from Facebook will be our guest and talk about his experiences and his life after graduation. After his talk, he will be answering the students’ questions.",
+  cta: "Register now!",
+};
+
+const sampleEvent2: EventData = {
+  date: sampleDate,
+  title: "KUrypto",
+  description:
+    "Ask Alumni is here once again! This time in Ask Alumni, Berker Soyluoğlu from Facebook will be our guest and talk about his experiences and his life after graduation. After his talk, he will be answering the students’ questions.",
+  cta: "Register now!",
+};
+
+const events: EventData[] = [sampleEvent, sampleEvent2];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      
+      {events.map((e) => <EventCard ed={e}/> )}
     </div>
   );
 }
 
 export default App;
+
+
