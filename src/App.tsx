0d81daa8ee9218b9784bed126d668e060e529dba
Michael Bora Sanuk <bora@aeontech.io>
2022-04-27 02:35:08 +0300

    refactored event data (now called KAEvent)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import EventCard, { EventData } from "./components/event-card/EventCard";
+import EventCard from "./components/event-card/EventCard";
 import Footer from "./components/footer/Footer";
-
-const sampleDate: string = "25 April, Monday • 20:30";
-
-const sampleEvent: EventData = {
-  datetime: sampleDate,
-  title: "Ask Alumni - Berker Soyluoğlu",
-  description:
-    "Ask Alumni is here once again! This time in Ask Alumni, Berker Soyluoğlu from Facebook will be our guest and talk about his experiences and his life after graduation. After his talk, he will be answering the students’ questions.",
-  cta: "Register now!",
-  img_src: "https://i.ibb.co/jgj5sfp/ask-alumni-final.png",
-};
-
-const sampleEvent2: EventData = {
-  datetime: sampleDate,
-  title: "Basics of LaTeX Workshop",
-  description:
-    "On April 27th, Ahmet Yesevi Nurcan from our club will be teaching the basics of LaTeX. If you want to build a career in Academics or want to create professionally written documents, don’t miss out on the opportunity and sign up for “Basics of LaTeX”!",
-  cta: "Register now!",
-  img_src: "https://i.ibb.co/SmdWMD9/final.png",
-};
-
-const events: EventData[] = [sampleEvent, sampleEvent2];
+import { sampleEvents } from "./data/KAEvent";
 
 function App() {
+  const events = sampleEvents;
   return (
     <div className="acm-app-container">
       <div className="acm-app-header">
@@ -34,7 +14,7 @@ function App() {
         <div className="acm-app-header-datetime">25 April - 1 May 2022</div>
       </div>
       {events.map((e) => (
-        <EventCard ed={e} />
+        <EventCard event={e} />
       ))}
       <Footer />
     </div>
---
+++ src/data/KAEvent.ts
@@ -0,0 +1,36 @@
+export type KAEvent = {
+  img_src: string;
+  title: string;
+  date: string;
+  time: string;
+  venue: string;
+  description: string;
+  cta: string;
+  cta_url: string;
+};
+
+const sampleEvent: KAEvent = {
+  img_src: "https://i.ibb.co/jgj5sfp/ask-alumni-final.png",
+  title: "Ask Alumni - Berker Soyluoğlu",
+  date: "25 April Monday",
+  time: "20:30",
+  venue: "Zoom",
+  description:
+    "Ask Alumni is here once again! This time in Ask Alumni, Berker Soyluoğlu from Facebook will be our guest and talk about his experiences and his life after graduation. After his talk, he will be answering the students’ questions.",
+  cta: "Register now!",
+  cta_url: "",
+};
+
+const sampleEvent2: KAEvent = {
+  img_src: "https://i.ibb.co/SmdWMD9/final.png",
+  title: "Basics of LaTeX Workshop",
+  date: "25 April Monday",
+  time: "20:30",
+  venue: "Zoom",
+  description:
+    "On April 27th, Ahmet Yesevi Nurcan from our club will be teaching the basics of LaTeX. If you want to build a career in Academics or want to create professionally written documents, don’t miss out on the opportunity and sign up for “Basics of LaTeX”!",
+  cta: "Register now!",
+  cta_url: "",
+};
+
+export const sampleEvents = [sampleEvent, sampleEvent2];
