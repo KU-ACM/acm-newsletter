b87b848a0527db00a895e61546ade8aee631fb55
Michael Bora Sanuk <bora@aeontech.io>
2022-04-29 21:00:41 +0300

    added edit toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-import EventCard from "./components/event-card/EventCard";
-import Footer from "./components/footer/Footer";
-import { sampleEvents } from "./data/KAEvent";
 import Newsletter from "./components/newsletter/Newsletter";
+import { sampleEvents } from "./data/KAEvent";
 
 function App() {
-  
+  const [isEditing, setEditing] = useState<boolean>(false);
+  const toggleEditing = () => setEditing(!isEditing);
   return (
-    <Newsletter date="25 April - 1 May 2022" events={sampleEvents}/>  
+    <>
+      <div className="acm-navigation-container">
+      <div className="acm-navigation-title">KU ACM Newsletter</div>
+        <div className="acm-button acm-button-small acm-button-light" onClick={toggleEditing}>
+          {isEditing ? "Preview" : "Edit"}
+        </div>
+      </div>
+      {isEditing ? (
+        <></>
+      ) : (
+        <Newsletter date="25 April - 1 May 2022" events={sampleEvents} />
+      )}
+    </>
   );
 }
 
