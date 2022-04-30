a4db28fac25adf9313e3eabf9746d26cfef1d476
Michael Bora Sanuk <bora@aeontech.io>
2022-04-30 16:14:27 +0300

    editor styling

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, useContext, useState } from "react";
 import { AppContext } from "../../data/AppContext";
 import { KAEvent } from "../../data/KANewsletter";
 import { uploadImage } from "../../services/ImageUploadService";
+import { CardImage } from "react-bootstrap-icons";
 import "./editor.css";
 
 const Editor = (): JSX.Element => {
@@ -21,8 +22,30 @@ const Editor = (): JSX.Element => {
     });
   };
 
+  const handleDateChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewsletterData({
+      date: e.target.value,
+      events: newsletterData.events,
+    });
+  };
+
   return (
     <div className="acm-editor-container">
+      <div className="acm-editor-title">Newsletter Editor</div>
+      <div className="acm-editor-subtitle">Date</div>
+      <div className="acm-editor-form-container">
+        <form className="acm-editor-form">
+          <input
+            type="text"
+            required
+            className="acm-editor-form-file-input"
+            style={{ margin: 0 }}
+            placeholder="Newsletter date"
+            onChange={handleDateChange}
+          />
+        </form>
+      </div>
+      <div className="acm-editor-subtitle">Events</div>
       <div className="acm-editor-event-preview-container">
         {newsletterData.events.map((e) => (
           <div className="acm-editor-event-preview" key={e.title}>
@@ -72,69 +95,97 @@ const EventForm = (props: EventFormProps): JSX.Element => {
   const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.currentTarget.files?.[0];
     if (file) {
-      uploadImage(file).then((result) => {setEventImgSrc(result);
+      uploadImage(file).then((result) => {
+        setEventImgSrc(result);
       });
     }
   };
 
   return (
-    <div className="acm-editor-event-form-container">
-      <div className="acm-editor-event-form-title">New Event</div>
+    <div className="acm-editor-form-container">
+      <div className="acm-editor-form-title">New Event</div>
       <form
-        className="acm-editor-event-form"
+        className="acm-editor-form"
         onSubmit={(e) => {
           e.preventDefault();
           handleSave();
         }}
       >
-        <div className="acm-editor-event-form-label">Image URL</div>
-        <input
-          required
-          type="file"
-          placeholder="Image URL"
-          onChange={handleImageUpload}
-        />
-        <div className="acm-editor-event-form-label">Title</div>
+        <div className="acm-editor-form-label">Image</div>
+        <div className="acm-editor-form-file-input-container">
+          <div className="acm-editor-form-file-image-preview">
+            {eventImgSrc ? (
+              <img
+                src={eventImgSrc}
+                className="acm-editor-form-file-image"
+                alt=""
+              />
+            ) : (
+              <CardImage size={32} color="#424242" />
+            )}
+          </div>
+          <div className="acm-editor-form-file-input-upload-area">
+            <div className="acm-editor-form-file-url">
+              <div style={{ fontWeight: "bold" }}>Image URL: </div>
+              {eventImgSrc ? (
+                <a href={eventImgSrc}>{eventImgSrc}</a>
+              ) : (
+                <div style={{ fontStyle: "italic" }}>No file selected</div>
+              )}
+            </div>
+            <div className="acm-editor-form-file-input-button">
+              <div>Choose file</div>
+              <input
+                required
+                className="acm-editor-form-file-input"
+                accept="image/png, image/jpeg"
+                type="file"
+                onChange={handleImageUpload}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="acm-editor-form-label">Title</div>
         <input
           required
           type="text"
           placeholder="Title"
           onChange={(e) => setEventTitle(e.target.value)}
         />
-        <div className="acm-editor-event-form-label">Date</div>
+        <div className="acm-editor-form-label">Date</div>
         <input
           required
           type="text"
           placeholder="Date"
           onChange={(e) => setEventDate(e.target.value)}
         />
-        <div className="acm-editor-event-form-label">Time</div>
+        <div className="acm-editor-form-label">Time</div>
         <input
           required
           type="text"
           placeholder="Time"
           onChange={(e) => setEventTime(e.target.value)}
         />
-        <div className="acm-editor-event-form-label">Venue</div>
+        <div className="acm-editor-form-label">Venue</div>
         <input
           required
           type="text"
           placeholder="Venue"
           onChange={(e) => setEventVenue(e.target.value)}
         />
-        <div className="acm-editor-event-form-label">Description</div>
+        <div className="acm-editor-form-label">Description</div>
         <textarea
           required
           placeholder="Description"
           onChange={(e) => setEventDescription(e.target.value)}
         />
-        <div className="acm-editor-event-form-label">CTA</div>
+        <div className="acm-editor-form-label">CTA</div>
         <input
           type="text"
           placeholder="CTA"
           onChange={(e) => setEventCTA(e.target.value)}
         />
-        <div className="acm-editor-event-form-label">CTA URL</div>
+        <div className="acm-editor-form-label">CTA URL</div>
         <input
           type="text"
           placeholder="CTA URL"
